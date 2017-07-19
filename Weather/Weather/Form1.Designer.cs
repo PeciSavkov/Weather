fb6f360fc64b292af8dceabf2b675632ecc04bf3
PeciSavkov <ptrsavkov@gmail.com>
2017-07-19 12:54:54 +0300

    Project

---
+++ Weather/Weather/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDt = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbCity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pbWeather = new System.Windows.Forms.PictureBox();
+            this.lblPresure = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblHumidity = new System.Windows.Forms.Label();
+            this.lblWindSpeed = new System.Windows.Forms.Label();
+            this.lblDes = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWeather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDt
+            // 
+            this.lblDt.AutoSize = true;
+            this.lblDt.Location = new System.Drawing.Point(68, 65);
+            this.lblDt.Name = "lblDt";
+            this.lblDt.Size = new System.Drawing.Size(35, 13);
+            this.lblDt.TabIndex = 0;
+            this.lblDt.Text = "label1";
+            this.lblDt.Visible = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(118, 23);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Търси";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbCity
+            // 
+            this.tbCity.Location = new System.Drawing.Point(12, 25);
+            this.tbCity.Name = "tbCity";
+            this.tbCity.Size = new System.Drawing.Size(100, 20);
+            this.tbCity.TabIndex = 2;
+            this.tbCity.Text = "Русе";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Въведете град за времето:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Времето в момента";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Град:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Температура:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Влажност";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Атмосферно налягане:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 220);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Вятър:";
+            // 
+            // pbWeather
+            // 
+            this.pbWeather.Location = new System.Drawing.Point(12, 76);
+            this.pbWeather.Name = "pbWeather";
+            this.pbWeather.Size = new System.Drawing.Size(50, 50);
+            this.pbWeather.TabIndex = 10;
+            this.pbWeather.TabStop = false;
+            // 
+            // lblPresure
+            // 
+            this.lblPresure.AutoSize = true;
+            this.lblPresure.Location = new System.Drawing.Point(142, 197);
+            this.lblPresure.Name = "lblPresure";
+            this.lblPresure.Size = new System.Drawing.Size(35, 13);
+            this.lblPresure.TabIndex = 11;
+            this.lblPresure.Text = "label1";
+            this.lblPresure.Visible = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(142, 129);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 12;
+            this.lblName.Text = "label1";
+            this.lblName.Visible = false;
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Location = new System.Drawing.Point(142, 151);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(35, 13);
+            this.lblTemp.TabIndex = 13;
+            this.lblTemp.Text = "label1";
+            this.lblTemp.Visible = false;
+            // 
+            // lblHumidity
+            // 
+            this.lblHumidity.AutoSize = true;
+            this.lblHumidity.Location = new System.Drawing.Point(142, 174);
+            this.lblHumidity.Name = "lblHumidity";
+            this.lblHumidity.Size = new System.Drawing.Size(35, 13);
+            this.lblHumidity.TabIndex = 14;
+            this.lblHumidity.Text = "label1";
+            this.lblHumidity.Visible = false;
+            // 
+            // lblWindSpeed
+            // 
+            this.lblWindSpeed.AutoSize = true;
+            this.lblWindSpeed.Location = new System.Drawing.Point(142, 220);
+            this.lblWindSpeed.Name = "lblWindSpeed";
+            this.lblWindSpeed.Size = new System.Drawing.Size(35, 13);
+            this.lblWindSpeed.TabIndex = 15;
+            this.lblWindSpeed.Text = "label1";
+            this.lblWindSpeed.Visible = false;
+            // 
+            // lblDes
+            // 
+            this.lblDes.AutoSize = true;
+            this.lblDes.Location = new System.Drawing.Point(68, 92);
+            this.lblDes.Name = "lblDes";
+            this.lblDes.Size = new System.Drawing.Size(35, 13);
+            this.lblDes.TabIndex = 16;
+            this.lblDes.Text = "label1";
+            this.lblDes.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 248);
+            this.Controls.Add(this.lblDes);
+            this.Controls.Add(this.lblWindSpeed);
+            this.Controls.Add(this.lblHumidity);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblPresure);
+            this.Controls.Add(this.pbWeather);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCity);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblDt);
+            this.Name = "Form1";
+            this.Text = "Времето";
+            ((System.ComponentModel.ISupportInitialize)(this.pbWeather)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDt;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbCity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pbWeather;
+        private System.Windows.Forms.Label lblPresure;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblHumidity;
+        private System.Windows.Forms.Label lblWindSpeed;
+        private System.Windows.Forms.Label lblDes;
+    }
+}
+
---
+++ Weather/Weather/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Weather
+{
+    public partial class Form1 : Form
+    {
+        HttpClient client = new HttpClient();
+        string url1;
+        string url2;
+        string json;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbCity.Text == "")
+            {
+                MessageBox.Show("Полето за град не може да бъде празно!");
+                return;
+            }
+            try
+            {
+                url1 = "http://api.openweathermap.org/data/2.5/weather?q=" + tbCity.Text + ",&appid=62326bbf104d6025a0bad581efe42a7e&units=metric&lang=bg";
+                json = client.GetStringAsync(url1).Result;
+                
+            }
+            catch
+            {
+                MessageBox.Show("Заявката неможа да се изпълни");
+                return;
+            }
+            cWeather wea = JsonConvert.DeserializeObject<cWeather>(json);
+            lblName.Text = tbCity.Text;
+            lblName.Visible = true;
+            lblTemp.Text = Convert.ToString(wea.main.temp) + " °C";
+            lblTemp.Visible = true;
+            lblHumidity.Text = Convert.ToString(wea.main.humidity) + " %";
+            lblHumidity.Visible = true;
+            lblPresure.Text = Convert.ToString(wea.main.pressure) + " hpa";
+            lblPresure.Visible = true;
+            lblWindSpeed.Text = Convert.ToString(wea.wind.speed + " m/s");
+            lblWindSpeed.Visible = true;
+            lblDes.Text = Convert.ToString(wea.weather.ElementAt(0).description);
+            lblDes.Visible = true;
+            url2 = "http://openweathermap.org/img/w/" + Convert.ToString(wea.weather.ElementAt(0).icon + ".png");
+            pbWeather.Load(url2);
+
+
+
+            DateTime time = DateTime.Now;
+            lblDt.Text = Convert.ToString(time);
+            lblDt.Visible = true;
+        }
+    }
+}
